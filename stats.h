424176d1d7528e91979e13bb7580f82f8bf9c6d9
Hemanth Sachithanandam <sachithanandamhemanth97@gmail.com>
2022-05-18 15:46:00 +0530

    Creating a struct variable

--- stats.h
+++ stats.h
@@ -1,5 +1,13 @@
 #include <vector>
 
+struct Stats
+{
+    /* data */
+    float average;
+    float max;
+    float min;
+};
+
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<___>& );
 }
