74f384a656bcddfdffbb4cbe79b9a43f280a14b9
Hemanth Sachithanandam <sachithanandamhemanth97@gmail.com>
2022-05-21 11:31:15 +0530

    FUnctions defined

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,30 @@
 #include "stats.h"
+#include <algorithm>
+#include <numeric>
+namespace Statistics {
+    Stats ComputeStatistics(const std::vector<double>& f_numbers) {
+        //Implement statistics here
+        Stats var_1;
+        sort(f_numbers.begin(), f_numbers.end());
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+        var_1.max = *max_element(f_numbers.begin(), f_numbers.end());
+        var_1.min = *min_element(f_numbers.begin(), f_numbers.end());
+        var_1.average = std::accumulate(f_numbers.begin(), f_numbers.end(),0.0)/f_numbers.size();
+        return var_1;
+    }
+}
+
+void StatsAlerter::checkAndAlert(std::initializer_list<double> f_array)
+{
+    std::vector<IAlerter*> alerters;
+    float maxThreshold;
+    StatsAlerter l_stats_obj(maxThreshold,alerters);
+    for(int i=0;i<sizeof(f_array);i++)
+    {
+        if(f_array[i]>=l_stats_obj.mT)
+        {
+            l_stats_obj.alert[0]->emailSent=true;
+            l_stats_obj.alert[1]->ledGlows=true;
+        }
+    }
 }
--- stats.h
+++ stats.h
@@ -3,11 +3,45 @@
 struct Stats
 {
     /* data */
-    float average;
-    float max;
-    float min;
+    double average;
+    double max;
+    double min;
 };
 
+class IAlerter{
+public:
+
+};
+
+class EmailAlert : public IAlerter{
+public:
+    bool emailSent;
+    //constructor
+    EmailAlert(){
+    emailSent = false;
+    }
+};
+
+class LEDAlert : public IAlerter{
+public:
+    bool ledGlows;
+    //constructor
+    LEDAlert(){
+    ledGlows = false;
+    }
+};
+
+class StatsAlerter {
+public:
+    double mT;
+    std::vector<IAlerter*> alert;
+    StatsAlerter(float maxThreshold,std::vector<IAlerter*> alerters)
+    {
+        mT = maxThreshold;
+        alert = alerters;
+    }
+    void checkAndAlert(std::initializer_list<double> f_array);
+};
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>& f_numbers);
 }
